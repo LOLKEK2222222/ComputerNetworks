ee2ae95e99913df58ea7916763dfe5e4e8478d93
Томилов Роман Олегович <167750768+LOLKEK2222222@users.noreply.github.com>
2025-02-18 14:40:35 +0700

    Update ping.py

--- lab1/ping.py
+++ lab1/ping.py
@@ -4,12 +4,10 @@ import os
 with open("ip_list.txt") as file:
     park = file.read().splitlines()
 
-file = open("ip_output.csv","w")
-
-for ip in park:
-    response = os.popen(f"ping -c 5 {ip} ").read()
-    postresponse = response.split('\n')[6:]
-    file.write(postresponse[1] + "\n")
-    file.write(postresponse[3] + "\n" + "\n")
-    print(postresponse)
-file.close()
+with open("ip_output.csv","w") as file:
+    for ip in park:
+        response = os.popen(f"ping -c 5 {ip} ").read()
+        postresponse = response.split('\n')[6:]
+        file.write(postresponse[1] + "\n")
+        file.write(postresponse[3] + "\n" + "\n")
+        print(postresponse)
